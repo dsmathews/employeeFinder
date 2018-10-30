bdf791fe94b9c35a0fd8932378e3130815ca4038
Darrell Mathews <smathewsbd@gmail.com>
2018-10-30 09:15:47 -0400

    Changes to html and server js.

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const path = require('path');
 const app = express();
 
 const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+    console.log(`App is now listening on PORT ${PORT}`);
+  });
