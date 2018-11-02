3cfdbd45ec37181d01ad311f478d7652599f28a7
Darrell Mathews <smathewsbd@gmail.com>
2018-11-02 09:33:50 -0400

    chenges to path files

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports= function (app) {
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, "./home.html"));
+    response.sendFile(path.join(__dirname, "../public/home.html"));
   });
 app.get('/survey', function(request, response) {
-    response.sendFile(path.join(__dirname, "./survey.html"));
+    response.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 };
