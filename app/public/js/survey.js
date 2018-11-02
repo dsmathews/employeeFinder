9d266e6ae6154d72f6d864379afc0659922e88a6
Darrell Mathews <smathewsbd@gmail.com>
2018-11-02 13:51:39 -0400

    changes: added link to survey.js in html head, corrected survey.js

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,54 +1,55 @@
-$('document').ready(function (){
-  const formValidation = function () {
-    let valid = true;
-    const alarm = function () {
-      alert('Uh oh, something happened. Please check your answers and re-submit.');
-    };
-    if ($('#name').val()) {
-      valid = false;
-      alarm();
-    }
-    else if ($('#pic').val()) {
-      valid = false;
-      alarm();
-    }
-    else if ($('.custom-select').val(NaN)) {
-      valid = false;
-      alarm();
+$(function () {
+  const validate = function () {
+    let isValid = true;
+
+    $('input').each(function () {
+      if (!$(this).val()) {
+        isValid = false;
+      }
+    });
+
+    $('.custom-select').each(function (i, element) {
+      if (!$(this).val()) {
+        isValid = false;
+      }
+    });
+
+    return isValid;
+  };
+
+  const displayWinner = function (data) {
+    $('#pair-name').text(data.name);
+    $('#pair-img').attr('src', data.photo);
+    $('#results-modal').modal('toggle');
+  };
+
+  const submit = function () {
+    event.preventDefault();
+    if (validate()) {
+      const userData = {
+        name: $('#name').val.trim,
+        photo: $('#photo').val.trim,
+        scores: [
+          $('#q1').val(),
+          $('#q2').val(),
+          $('#q3').val(),
+          $('#q4').val(),
+          $('#q5').val(),
+          $('#q6').val(),
+          $('#q7').val(),
+          $('#q8').val(),
+          $('#q9').val(),
+          $('#q10').val()
+        ]
+      };
+      $.post('/api/employees', userData, displayWinner);
     }
     else {
-      valid = true;
-      const submit = function () {
-        const userData = {
-          name: $('#name').val.trim,
-          photo: $('#photo').val.trim,
-          scores: [
-            $('#q1').val(),
-            $('#q2').val(),
-            $('#q3').val(),
-            $('#q4').val(),
-            $('#q5').val(),
-            $('#q6').val(),
-            $('#q7').val(),
-            $('#q8').val(),
-            $('#q9').val(),
-            $('#q10').val()
-          ]
-        };
-        const displayWinner = function(data) {
-          $('#pair-name').text(data.name);
-          $('#pair-img').attr('src', data.photo);
-          $('#results-modal').modal('toggle');
-        };
-        $.post('/api/employees', userData, displayWinner);
-      };
-      
-      
-
-      $("#submit").on('click', submit);
-
+      $('#error')
+        .text('Uh oh, please check your answers before submitting')
+        .addClass('alert alert-warning');
     }
+};
 
-  };
-  formValidation();
+$("#submit").on('click',submit);
 });
